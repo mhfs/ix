160b3fce82b23535bf1e61fa715f5d0c14065ddd
Marcelo Silveira <marcelo@mhfs.com.br>
2015-01-31 09:05:38 -0800

    fetch and print issues

--- thisweek.go
+++ thisweek.go
@@ -3,8 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
+	"time"
 
+	"code.google.com/p/goauth2/oauth"
 	"github.com/codegangsta/cli"
+	"github.com/google/go-github/github"
 )
 
 func init() {
@@ -68,7 +72,43 @@ func main() {
 		}
 
 		fmt.Printf("Starting Processing for repo '%s'\n", repo)
+
+		parts := strings.Split(repo, "/")
+		issues, err := loadIssues(parts[0], parts[1])
+
+		if err != nil {
+			panic(err) // FIXME be smarter. handle 404s, 403s, ...
+		}
+
+		for _, issue := range issues {
+			printIssue(&issue)
+		}
 	}
 
 	app.Run(os.Args)
+}
+
+func loadIssues(owner string, repo string) ([]github.Issue, error) {
+	t := &oauth.Transport{
+		Token: &oauth.Token{AccessToken: os.Getenv("GH_TOKEN")},
+	}
+
+	client := github.NewClient(t.Client())
+
+	from, err := time.Parse("2006-01-02", "2015-01-25")
+
+	if err != nil {
+		panic("invalid date provided")
+	}
+
+	options := github.IssueListByRepoOptions{State: "closed", Sort: "updated", Since: from}
+	issues, _, err := client.Issues.ListByRepo(owner, repo, &options)
+
+	return issues, err
+}
+
+func printIssue(issue *github.Issue) {
+	// TODO handle nils and missing assignee
+	// fmt.Printf("#%d %s %s (%s)", issue.Number, issue.ClosedAt, issue.Title, issue.Assignee.Login)
+	fmt.Printf("#%d - %s - %s\n", *issue.Number, issue.ClosedAt, *issue.Title)
 }
